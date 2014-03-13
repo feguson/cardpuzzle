1fdf73aaae4eb23c1cff699dae6ac52af4f2503e
Javier Bórquez <hola@javierbyte.com>
2014-03-13 04:21:50 -0700

    fixed parsing

--- www/code.js
+++ www/code.js
@@ -18,7 +18,7 @@ main.style.webkitPerspective = size + 'px';
 
 //initializing
 var lvl = [];
-lvl[0] = '0000000100011100010000000';
+lvl[0] = '0100000100011100010000000';
 
 var l = []
 for(x=0;x<5;x++) {
@@ -29,26 +29,29 @@ for(x=0;x<5;x++) {
 }
 
 function load(k) {
-	for(x=0;x<5;x++) for(y=0;y<5;y++) {
-		if(l[x][y] != lvl[k].charAt(0)) {
-			console.log(x + ' ' + y);
+	newlevel = lvl[k];
+
+	for(y=0;y<5;y++) for(x=0;x<5;x++) {
+		if(l[x][y] != newlevel.charAt(0)) {
 			document.getElementById('c' + x + '' + y).style.webkitTransform  += 'rotateY(180deg) ';
 		}
 
-		l[x][y] = lvl[k].charAt(0);
-		lvl[k] = lvl[k].substring(1);
+		l[x][y] = parseInt(newlevel.charAt(0));
+		newlevel = newlevel.substring(1);
 	}
 }
 
 function go(i,j) {
 	l[i][j] = !l[i][j];
 	document.getElementById('c' + i + '' + j).style.webkitTransform  += 'rotateY(180deg) ';
+
 	for(x=-1;x<2;x+=2) {
 		if(0<=i+x && i+x<=4) {
 			l[i+x][j] = !l[i+x][j];
 			document.getElementById('c' + (i+x) + '' + j).style.webkitTransform  += 'rotateY(' + (180*x) + 'deg) ';
 		}
 		if(0<=j+x && j+x<=4) {
+			l[i][j+x] = !l[i][j+x];
 			document.getElementById('c' + i + '' + (j+x)).style.webkitTransform  += 'rotateX(' + (-180*x) + 'deg) ';
 		}
 	}
