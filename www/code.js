0660b31fd95b16333279be6c7529036df141ecf7
Javier Bórquez <hola@javierbyte.com>
2014-03-14 15:01:04 -0700

    a lot of news
    saves last level
    auto start with the last level
    added back and next buttons
    fixed click count
    fixed name on the start

--- www/code.js
+++ www/code.js
@@ -18,39 +18,47 @@ main.style.marginLeft = (wW - size)/2 + 'px';
 main.style.marginTop = (wH - size)/2 + 'px';
 main.style.webkitPerspective = size + 'px';
 
-// UI functions
-
-function msjhide() {
-	document.getElementById('msj').className = "hide";
-	load(currentlevel-1);
-}
-
 //initializing
 var currentlevel = 1;
+var record = 1;
 var clicks = 0;
 
 var lvl = [];
-lvl[0] = '0000000100011100010000000';
-lvl[1] = '0001100001010001110001000';
-lvl[2] = '0100011100010100011100010';
-lvl[3] = '1110001010001111001011000';
-lvl[4] = '0000000000001001111111111';
-lvl[5] = '0000001110101010111000000';
-lvl[6] = '0010000100111110101000000';
-lvl[7] = '1101110101011101010011000';
-lvl[8] = '0100011100010101011111010';
-lvl[9] = '0101011011000001101101010';
-lvl[10] = '1101101010000000101011011';
-lvl[11] = '0110010010010100100100110';
-lvl[12] = '0000100010001000100010000';
-lvl[13] = '0010000100110110010000100';
-lvl[14] = '0101001010000000101001010';
-lvl[15] = '0111011111110111111101110';
-lvl[16] = '0101010001000001000101010';
-lvl[17] = '0111011011101011101101110';
-lvl[18] = '0101001110111111010100000';
-lvl[19] = '1000101110001000000001110';
-lvl[20] = '0111011011111110000011111';
+lvl[1] = '0000000100011100010000000';
+lvl[2] = '0001100001010001110001000';
+lvl[3] = '0100011100010100011100010';
+lvl[4] = '1110001010001111001011000';
+lvl[5] = '0000000000001001111111111';
+lvl[6] = '0000001110101010111000000';
+lvl[7] = '0010000100111110101000000';
+lvl[8] = '1101110101011101010011000';
+lvl[9] = '0100011100010101011111010';
+lvl[10] = '0101011011000001101101010';
+lvl[11] = '1101101010000000101011011';
+lvl[12] = '0110010010010100100100110';
+lvl[13] = '0000100010001000100010000';
+lvl[14] = '0010000100110110010000100';
+lvl[15] = '0101001010000000101001010';
+lvl[16] = '0111011111110111111101110';
+lvl[17] = '0101010001000001000101010';
+lvl[18] = '0111011011101011101101110';
+lvl[19] = '0101001110111111010100000';
+lvl[20] = '1000101110001000000001110';
+lvl[21] = '0111011011111110000011111';
+var originallevels = lvl.length;
+
+//is this a new record?
+if(localStorage["maxrecord"] > 0) {
+	record = parseInt(localStorage["maxrecord"],10);
+	currentlevel = record;
+
+	document.getElementById("msj-title").innerHTML = "Bienvenido de nuevo";
+	document.getElementById("msj-txt").innerHTML = "Continua en el nivel " + record + ".";
+
+	for(x=originallevels;x<=record;x++) lvl[x] = localStorage["lvl"+x];
+} else {
+	localStorage["maxrecord"] = 0;
+}
 
 var l = []
 for(x=0;x<5;x++) {
@@ -61,9 +69,22 @@ for(x=0;x<5;x++) {
 }
 
 function load(k) {
+	if(k > record) {
+		record = k;
+		localStorage["maxrecord"] = parseInt(record,10);
+	}
+	buttons(k);
+
 	clicks = 0;
 	last = Date.now();
-	if(k<19) {
+
+	document.getElementById("level").innerHTML = (k);
+
+	if(k<lvl.length) {
+		
+		if(k>20) main.className = "max";
+		else main.className = "normal";
+
 		newlevel = lvl[k];
 	
 		for(y=0;y<5;y++) for(x=0;x<5;x++) {
@@ -73,25 +94,46 @@ function load(k) {
 	
 			l[x][y] = parseInt(newlevel.charAt(0));
 			newlevel = newlevel.substring(1);
+
+			currentlevel = k + 1;
+			last = Date.now();
 		}
 	} else {
-		main.className = "max";
-		for(x=0;x<5;x++) for(y=0;y<5;y++) l[x][y] = 0;
-
-		for(x=0;x<(1+Math.sqrt(Math.sqrt(k)))|0;x++) {
-			i = ( Math.random()*5 ) | 0;
-			j = ( Math.random()*5 ) | 0;
-			if(l[i][j]!=1) go(i,j);
-		}
+		lastlevel = lvl.length;
+		lvl[lastlevel] = "";
+		for(x=0;x<25;x++) lvl[lastlevel] += (Math.random()+.5)|0;
+		localStorage["lvl"+lastlevel] = lvl[lastlevel];
+		load(lastlevel);
 	}
+}
 
-	last = Date.now();
+//ui constrols
+function msjhide() {
+	document.getElementById('msj').className = "hide";
+	load(currentlevel);
 }
 
 function back() {
+	if(currentlevel>2) load(currentlevel-2);
+}
+
+function restart() {
 	load(currentlevel-1);
 }
 
+function next() {
+	if(currentlevel<=record) load(currentlevel);
+}
+
+function buttons(k) {
+	if(k<=1) document.getElementById("icon-back").style.opacity = "0";
+	else document.getElementById("icon-back").style.opacity = "1";
+
+	if(k>record-1) document.getElementById("icon-next").style.opacity = "0";
+	else document.getElementById("icon-next").style.opacity = "1";
+}
+
+//manage click
 function go(i,j) {
 	clicks++;
 	l[i][j] = !l[i][j];
@@ -108,7 +150,7 @@ function go(i,j) {
 		}
 	}
 
-	if( check() ) next();
+	if( check() ) nextlevel();
 }
 
 function check() {
@@ -131,16 +173,12 @@ function generate() {
 	console.log(cache);
 }
 
-function next() {
+function nextlevel() {
 	now = Date.now();
 	passed = (((now-last)/100)|0)/10;
-	document.getElementById('msj-title').innerHTML = "Felicidades, pasaste al nivel " + (currentlevel+1);
+	document.getElementById('msj-title').innerHTML = "Felicidades, pasaste al nivel " + (currentlevel);
 	document.getElementById('msj-txt').innerHTML = "Solo te tomó " + clicks + " click";
 	if(clicks>1) document.getElementById('msj-txt').innerHTML += "s";
 	document.getElementById('msj-txt').innerHTML += " y " + passed + " segundos."
 	document.getElementById('msj').className = "";
-	currentlevel++;
-	clicks = 0;
 }
-
-load(0);
