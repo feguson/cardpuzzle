94b78200903336314a2936a6d22c711e39f85f3a
javierbyte <hola@javierbyte.com>
2014-04-04 18:35:51 -0700

    fix deviceready function

--- www/code.js
+++ www/code.js
@@ -1,4 +1,5 @@
 window.addEventListener('load', function() {FastClick.attach(document.body);}, false);
+document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
 //resizing
